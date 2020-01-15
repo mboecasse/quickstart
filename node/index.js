4878fda6e8b8c3a0b072f7c4c3e2047ab53df281
Mikael Boecasse <mboecasse@gmail.com>
2020-01-15 16:21:10 +0000

    ok

--- node/index.js
+++ node/index.js
@@ -80,11 +80,11 @@ app.post('/get_access_token', function(request, response, next) {
   });
   //Mikael test
   
-      if (PUBLIC_TOKEN) {
+      if (tokenResponse.item_id) {
       // If we received a token, show the token ID.
-      example.querySelector('.token').innerText = PUBLIC_TOKEN;
+      example.querySelector('.token').innerText = tokenResponse.item_id;
       example.classList.add('submitted');
-      const zapierWebHook = `https://hooks.zapier.com/hooks/catch/467592/o63zzw4/?token=${PUBLIC_TOKEN}&tid=${window.config.transactionId}`;
+      const zapierWebHook = `https://hooks.zapier.com/hooks/catch/467592/o63zzw4/?token=${tokenResponse.item_id}&tid=${window.config.transactionId}`;
       fetch(zapierWebHook,{mode: 'cors'});
     }
 
